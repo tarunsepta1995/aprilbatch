a63c3180b10534e251506dd6e85f8356758e014a
tarunsepta1995 <tarunsepta1995@gmail.com>
2026-04-08 15:07:03 +0530

    finish

--- terraform-assignment/modules/aws_iam/main.tf
+++ terraform-assignment/modules/aws_iam/main.tf
@@ -2,18 +2,6 @@ resource "aws_iam_role" "eks_role" {
   name = var.name
 
   assume_role_policy = jsonencode(var.role_policy)
-  #   assume_role_policy = jsonencode({
-  #     Version = "2012-10-17"
-  #     Statement = [
-  #       {
-  #         Effect = "Allow"
-  #         Principal = {
-  #           Service = "eks.amazonaws.com"
-  #         }
-  #         Action = "sts:AssumeRole"
-  #       }
-  #     ]
-  #   })
 }
 
 resource "aws_iam_policy_attachment" "eks_role_attachment" {
